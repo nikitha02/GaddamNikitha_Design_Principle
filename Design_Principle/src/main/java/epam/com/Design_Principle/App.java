531a3286c762ffd7ad5ae7b73fab63cce2e40e04
Nikitha <Nikitha@192.168.12.242>
2020-03-02 17:40:12 +0530

    created a new calculator

---
+++ Design_Principle/src/main/java/epam/com/Design_Principle/App.java
@@ -0,0 +1,13 @@
+package epam.com.Design_Principle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Design_Principle/src/main/java/epam/com/Design_Principle/MathOperations.java
@@ -0,0 +1,18 @@
+package epam.com.Design_Principle;
+
+public class MathOperations {
+
+	double add(double x, double y) {
+		return x + y;
+	}
+	double substract(double x, double y) {
+		return x - y;
+	}
+	double multiply(double x, double y) {
+		return x * y;
+	}
+	double divide(double x, double y) {
+		return x / y;
+	}
+
+}
